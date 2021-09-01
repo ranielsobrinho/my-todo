293566a5783f81f1e3bd6103fb6499875f6ffa53
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-01 06:47:03 -0300

    Main server file are now just for start the server

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-import express from "express";
-// npm install consign --save
-import consign from "consign";
-
-const PORT = 3000;
+const express  = require("express");
+const consign = require("consign");
 
 const app = express();
 
-app.set("json spaces", 4);
-
 consign()
 .include("models")
+.then("libs/middlewares.js")
 .then("routes")
+.then("libs/boot.js")
 .into(app);
-
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`));
