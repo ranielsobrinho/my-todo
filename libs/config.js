086073940c9525b2fad44488c65030b6c736125f
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-07 11:57:31 -0300

    Update to hind sensitive information

--- libs/config.js
+++ libs/config.js
@@ -1,8 +1,9 @@
+require('dotenv/config');
 module.exports = {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port:process.env.DB_PORT,
     dialect: 'postgres',
-    host: 'localhost',
-    user: 'dev',
-    password: '95751535r',
-    database: 'todos',
-    port:5432
+    host: process.env.DB_HOST
 }
