d35cb4733ea1833dd1fee97959b0d0a7dc34af23
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-07 11:31:19 -0300

    Listing all todos from db using async function

--- routes/main.js
+++ routes/main.js
@@ -1,5 +1,12 @@
+const db = require("../database/db")
 module.exports = app => {
-    app.get("/", (req, res) => {
-        res.json({status: "My API"});
+    app.get("/todos", async (req, res) => {
+        let resultado = await db.query("select * from todos",(error, result) => {
+            if(error){
+                res.send(error);
+            }
+            resultado = result.rows;
+            res.send(resultado);
+        })
     });
 }
