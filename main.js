1006ae678699885c75fb5444ab4a5c62272e9a0d
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-12 17:13:40 -0300

    Stopped usign consign for a while

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const express  = require("express");
 const consign = require("consign");
+const routes = require('./routes');
+require('./database/db');
 
 const app = express();
 app.use(express.json());
+app.use(routes)
 
-consign()
-.include("db.js")
-.then("models")
-.then("libs/middlewares.js")
-.then("routes")
-.then("libs/boot.js")
-.into(app);
+app.listen(3333);
+
+// consign()
+// .include("libs/config.js")
+// .then("db.js")
+// .then("libs/middlewares.js")
+// .then("routes.js")
+// .then("libs/boot.js")
+// .into(app);
