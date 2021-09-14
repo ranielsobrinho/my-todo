d47352f6cefeb6e42f0579e63cd3d11977277167
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-13 23:12:59 -0300

    Delete unused files

--- routes/todos.js
+++
@@ -1,9 +0,0 @@
-const db = require("../database/db")
-
-module.exports = app => {
-    app.get("/todos/:id", async (req, res) => {
-        const {id} = req.params
-        const {rows} = await db.getTodo('SELECT * FROM todos WHERE id = $1', [id]);
-        res.send(rows[0])
-    });
-}
