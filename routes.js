24e11b40b819edba3f7ba2125f719b04bcf8cf26
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-15 17:51:23 -0300

    Add new routes to update and delete

--- routes.js
+++ routes.js
@@ -6,8 +6,12 @@ const routes = express.Router();
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.delete('/users/:id', UserController.delete);
 
 routes.post('/users/:userId/todos', TodoController.store);
 routes.get('/users/:userId/todos', TodoController.index);
+routes.put('/users/:id/todos', TodoController.update);
+routes.delete('/users/:id/todos', TodoController.delete);
 
 module.exports = routes;
