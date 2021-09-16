130f7d5c67537c71eaa53ea1ae38d66532373d12
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-16 17:44:10 -0300

    Add passport module and jwt-simple

--- libs/config.js
+++ libs/config.js
@@ -1,9 +1,9 @@
 require('dotenv/config');
 module.exports = {
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
+    database: 'todos',
+    username: 'dev',
+    password: '95751535r',
+    port:5432,
     dialect: 'postgres',
-    host: process.env.DB_HOST
+    host: 'localhost'
 }
