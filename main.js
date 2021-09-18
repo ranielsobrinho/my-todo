49e07c5b2a3cc92a5a8795a52409d387ab4c3f3d
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-18 14:26:00 -0300

    Using cors and auth logic

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const express  = require("express");
-const routes = require('./routes');
+const routes = require('./routes/routes');
 require('./database/db');
+const cors = require('cors');
+const authorization =  require('./auth');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:["http://localhost:3000"],
+    methods:["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(routes)
+const auth = authorization(app);
+app.use(auth.initialize());
 
 app.listen(3333);
