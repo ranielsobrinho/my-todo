60b7844dd8632b5950f0d2b2b5afbe48d45731dc
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-18 14:29:52 -0300

    Moved to a new folder

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const TodoController = require('./controllers/TodoController');
-const UserController = require('./controllers/UserController');
+const TodoController = require('../controllers/TodoController');
+const UserController = require('../controllers/UserController');
+const AuthController = require('../controllers/AuthController');
 
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
+routes.get('/user', UserController.user);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
@@ -13,5 +15,7 @@ routes.post('/users/:userId/todos', TodoController.store);
 routes.get('/users/:userId/todos', TodoController.index);
 routes.put('/users/:id/todos', TodoController.update);
 routes.delete('/users/:id/todos', TodoController.delete);
+
+routes.post('/token', AuthController.token);
 
 module.exports = routes;
