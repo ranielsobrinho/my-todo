40fac619fba2abbd0c39be4ce9a2b9446f352943
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-23 19:35:14 -0300

    Comparing passwords to return a token

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,22 +1,38 @@
 const jwt = require('jwt-simple');
 const config = require('../libs/config');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async token(req, res){
-        const {email} = req.body;
+        const { email, password } = req.body;
 
         const userToken = await User.findOne({where:{email}})
         .then(user => {
-            if(!user){
-                res.status(400).json({error: 'User not found'});
+            if (!user) {
+                res.status(401).json({error: 'Falha na autenticação'});
             }
-            const payload = {id: user.id};
-            res.json({
-                token: jwt.encode(payload, config.jwtSecret)
-            })
+            (async () => {
+                // Load hash from your password DB.
+                const result1 = await bcrypt.compare(myPlaintextPassword, hash);
+            });
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (err) {
+                    return res.status(401).json({error: 'Falha na autenticação'});
+                }
+
+                if (!result) {
+                    return res.json({error: 'password invalid'});
+                }
+
+                if (result) {
+                    const payload = {id: user.id};
+                    return res.json({
+                        token: jwt.encode( payload, config.jwtSecret )
+                    })
+                }
+            });
         }).catch((error) => res.send(error))
 
-        return res.send(userToken);
-    },
+    }
 }
