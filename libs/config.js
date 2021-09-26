ac817f1c9ec5de6421b7c3cbfed7a832b6ffb5c0
ranielsobrinho <raniel25sobrinho@gmail.com>
2021-09-26 13:54:38 -0300

    Using enviroment variables

--- libs/config.js
+++ libs/config.js
@@ -1,9 +1,11 @@
 require('dotenv/config');
 module.exports = {
-    database: 'todos',
-    username: 'dev',
-    password: '95751535r',
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     port:5432,
     dialect: 'postgres',
-    host: 'localhost'
+    host: process.env.DB_HOST,
+    jwtSecret: process.env.JWT_KEY,
+    jwtSession: process.env.JWT_SESSION
 }
