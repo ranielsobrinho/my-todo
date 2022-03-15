106e11096fc63234744382b2ff6bd0160f167542
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-03-15 12:29:33 -0300

    code formatted

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const express  = require("express");
-const routes = require('./routes/routes');
-require('./database/db');
-const cors = require('cors');
+const express = require("express");
+const routes = require("./routes/routes");
+require("./database/db");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin:["http://localhost:3000"],
-    methods:["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.use(routes)
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(routes);
 
 app.listen(3333);
