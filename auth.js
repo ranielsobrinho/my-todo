c6e0d0d9a234999016c898532df2a336e3adead4
ranielsobrinho <raniel25sobrinho@gmail.com>
2022-03-15 12:28:45 -0300

    code formatted

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
-const jwt = require('jwt-simple');
+const jwt = require('jwt-simple')
 const key = require('./libs/config')
 
 module.exports = (req, res, next) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decode = jwt.decode(token, key.jwtSecret)
-        req.usuario = decode;
-        next();
-    } catch (error) {
-        return res.status(401).json({mensagem : 'Falha na autenticação'});
-    }
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+    const decode = jwt.decode(token, key.jwtSecret)
+    req.usuario = decode
+    next()
+  } catch (error) {
+    return res.status(401).json({ mensagem: 'Falha na autenticação' })
+  }
 }
